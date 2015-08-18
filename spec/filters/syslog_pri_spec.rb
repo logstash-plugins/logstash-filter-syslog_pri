c4f0e18a97f97a5615fe65dd310a2dcaafbf4c82
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-18 14:12:36 +0200

    add basic test for the plugin
    Fixes #2

--- spec/filters/syslog_pri_spec.rb
+++ spec/filters/syslog_pri_spec.rb
@@ -1,5 +1,44 @@
-require "logstash/devutils/rspec/spec_helper"
-require 'logstash/filters/syslog_pri'
+# encoding: utf-8
+require_relative "../spec_helper"
+require "logstash/plugin"
+require "logstash/event"
 
 describe LogStash::Filters::Syslog_pri do
+
+  subject          { LogStash::Filters::Syslog_pri.new( "syslog_pri_field_name" => "syslog_pri" ) }
+  let(:properties) { {:name => "foo" } }
+  let(:event)      { LogStash::Event.new(properties) }
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("filter", "syslog_pri").new( "facility_labels" => ["kernel"] )
+    expect { plugin.register }.to_not raise_error
+  end
+
+  describe "filtering" do
+
+    let(:msg) { "<1>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+
+    let(:properties) {{ "message" => msg }}
+
+    before(:each) do
+      subject.register
+    end
+
+     it "should syslog_facility be extracted" do
+      subject.filter(event)
+      expect(event["syslog_facility"]).to eq("user-level")
+    end
+
+    it "should syslog severity be extracted" do
+      subject.filter(event)
+      expect(event["syslog_severity"]).to eq("notice")
+    end
+
+    it "should syslog severity code be extracted" do
+      subject.filter(event)
+      expect(event["syslog_severity_code"]).to eq(5)
+    end
+
+  end
+
 end
