fb843ffbc9ba9f53dfa695108f4aaaafd2841985
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-24 12:54:07 +0200

    added a test for default values in syslog_pri and facilites
    Fixes #2

--- spec/filters/syslog_pri_spec.rb
+++ spec/filters/syslog_pri_spec.rb
@@ -24,7 +24,7 @@ describe LogStash::Filters::Syslog_pri do
       subject.register
     end
 
-     it "should syslog_facility be extracted" do
+    it "should syslog_facility be extracted" do
       subject.filter(event)
       expect(event["syslog_facility"]).to eq("user-level")
     end
@@ -37,6 +37,23 @@ describe LogStash::Filters::Syslog_pri do
     it "should syslog severity code be extracted" do
       subject.filter(event)
       expect(event["syslog_severity_code"]).to eq(5)
+    end
+
+  end
+
+  describe "defaults" do
+
+    let(:msg) { "<2> Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+
+    let(:properties) {{ "message" => msg }}
+
+    before(:each) do
+      subject.register
+    end
+
+    it "defaults priority to 13" do
+      subject.filter(event)
+      expect(event["syslog_facility_code"]).to eq(1)
     end
 
   end
