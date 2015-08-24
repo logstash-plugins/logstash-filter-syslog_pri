c6354c2fcb917ab76c2c31fd054a519206647076
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-24 14:54:19 +0200

    Add several new test cases to handle debug, notice, alert, etc kind of messages. Added also a grok parser to pull out fields out of lines of text.
    Fixes #2

--- spec/filters/syslog_pri_spec.rb
+++ spec/filters/syslog_pri_spec.rb
@@ -14,46 +14,99 @@ describe LogStash::Filters::Syslog_pri do
     expect { plugin.register }.to_not raise_error
   end
 
-  describe "filtering" do
+  describe "defaults" do
 
-    let(:msg) { "<1>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+    subject          { LogStash::Filters::Syslog_pri.new( "syslog_pri_field_name" => "my_syslog_pri" ) }
 
+    let(:msg)        { "<1>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
     let(:properties) {{ "message" => msg }}
+    let(:event)      { parse(LogStash::Event.new(properties)) }
 
     before(:each) do
       subject.register
     end
 
-    it "should syslog_facility be extracted" do
+    it "default syslog_facility is user-level" do
       subject.filter(event)
       expect(event["syslog_facility"]).to eq("user-level")
     end
 
-    it "should syslog severity be extracted" do
+    it "default syslog severity is notice" do
       subject.filter(event)
       expect(event["syslog_severity"]).to eq("notice")
     end
 
-    it "should syslog severity code be extracted" do
+    it "default severity to be 5, out of priority default 13" do
       subject.filter(event)
       expect(event["syslog_severity_code"]).to eq(5)
     end
 
   end
 
-  describe "defaults" do
-
-    let(:msg) { "<2> Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+  describe "filtering" do
 
     let(:properties) {{ "message" => msg }}
+    let(:event)      { parse(LogStash::Event.new(properties)) }
 
     before(:each) do
       subject.register
     end
 
-    it "defaults priority to 13" do
-      subject.filter(event)
-      expect(event["syslog_facility_code"]).to eq(1)
+    context "when critical messages arrive" do
+      let(:msg)        { "<34>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+
+      it "syslog severity is critical" do
+        subject.filter(event)
+        expect(event["syslog_severity"]).to eq("critical")
+      end
+
+      it "default syslog_facility is user-level" do
+        subject.filter(event)
+        expect(event["syslog_facility"]).to eq("security/authorization")
+      end
+
+    end
+
+    context "when notice local messages arrive" do
+      let(:msg)        { "<165>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+
+      it "syslog severity is notice" do
+        subject.filter(event)
+        expect(event["syslog_severity"]).to eq("notice")
+      end
+
+      it "default syslog_facility is user-level" do
+        subject.filter(event)
+        expect(event["syslog_facility"]).to eq("local4")
+      end
+    end
+
+    context "when a debug messages arrive" do
+      let(:msg)        { "<191>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+
+      it "syslog severity is notice" do
+        subject.filter(event)
+        expect(event["syslog_severity"]).to eq("debug")
+      end
+
+      it "default syslog_facility is user-level" do
+        subject.filter(event)
+        expect(event["syslog_facility"]).to eq("local7")
+      end
+    end
+
+    context "when an alert messages arrive" do
+      let(:msg)        { "<137>Sep 20 02:58:12 porridge3 puppet-master[27025]: Compiled catalog for bigbopper.adm.intranet in environment production in 1.31 seconds\n" }
+
+      it "syslog severity is notice" do
+        subject.filter(event)
+        expect(event["syslog_severity"]).to eq("alert")
+      end
+
+      it "default syslog_facility is user-level" do
+        subject.filter(event)
+        expect(event["syslog_facility"]).to eq("local1")
+      end
     end
 
   end
